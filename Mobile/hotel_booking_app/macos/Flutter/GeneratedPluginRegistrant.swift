d1b658c1dcc14bc479a405088c25178deeb9bc64
dzkukic <dzkukic.dev@gmail.com>
2026-08-17 23:12:38 +0200

    feat: Update project configuration and dependencies for improved compatibility

--- Mobile/hotel_booking_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Mobile/hotel_booking_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,14 +6,14 @@ import FlutterMacOS
 import Foundation
 
 import app_links
-import flutter_secure_storage_macos
+import flutter_secure_storage_darwin
 import path_provider_foundation
 import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
